13a14a29e7f560deca010f4dcb88e1bcbe7b836f
Joël Kuijten <joel@pm5544.eu>
2018-05-01 09:49:56 +0200

    Fixed some typo's in comments

--- src/js/bg/user.js
+++ src/js/bg/user.js
@@ -66,39 +66,40 @@ class UserModule {
         this.app.api.client.post('api/permission/apitoken/', payload)
             .then(({status, data, data: { apitoken, api_token }}) => {
 
-                // be aware that the response from api/permissions/apitoken is very confusing
+                // Be aware that the response from api/permissions/apitoken is very confusing
                 // it returns an api_token property when successful
                 // it returns an apitoken object when input is invalid or needs more data
                 const token = api_token
                 const errors = apitoken;
 
-                // ok let's start on our journey...
+                // Ok let's start on our journey...
                 if (this.app.api.NOTOK_STATUS.includes(status)) {
 
-                    // when username or password didn't match their names are both in the errors object
+                    // When username or password didn't match their names are both in the errors object
                     if (errors.username && errors.password) {
                         this.clearTemporaryData()
                         this.app.emit('user:login.failed', {reason: status})
+
                     } else if (errors.two_factor_token) {
-                        // if the error has something to do with the 2fa token it can be found in the 'two_factor_token' property
+                        // If the error has something to do with the 2fa token it can be found in the 'two_factor_token' property
 
                         const [twoFactorErrorMessage] = errors.two_factor_token
 
-                        // the error can either mean the 2fa token is mandatory and needds ot be supplied
+                        // The error can either mean the 2fa token is mandatory and needds ot be supplied
                         if (twoFactorErrorMessage.toLowerCase().includes('required')) {
                             this.app.emit('user:login.twoFactorMandatory')
                         } else {
-                            // or it can mean the 2fa token that was provided is incorrect
+                            // Or it can mean the 2fa token that was provided is incorrect
                             this.app.emit('user:twoFactorLogin.failed')
                         }
                     } else {
-                        // citchen sink for all other situations
+                        // Kitchen sink for all other situations
                         this.app.emit('user:login.failed', {reason: status})
                     }
                 } else if (this.app.api.OK_STATUS.includes(status)) {
                     if (token) {
                         this.app.store.set('token', `Token ${username}:${token}`)
-                        // after the token is stored we can call services since the token is automatically added to the requests
+                        // After the token is stored we can call services since the token is automatically added to all the requests
                         return this.app.api.client.get('api/permission/systemuser/profile/')
                     }
                 }
